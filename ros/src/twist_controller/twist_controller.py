88dc86155fd34a3bc9fc1fe81ee7dc17bca9e904
Joey Li <lijiayi17@gmail.com>
2019-12-01 23:37:04 -0500

    finish control()

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,8 +20,8 @@ class Controller(object):
         max_gas = 0.2
         self.throttle_controller = PID(kp, ki, kd, min_gas, max_gas)
 
-        tau = 0.5
-        ts = 0.02
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
+        ts = 0.02 # Sample time
         self.vel_lpf = LowPassFilter(tau, ts)
 
         self.decel_limit    = decel_limit
@@ -31,11 +31,34 @@ class Controller(object):
         self.fuel_capacity  = fuel_capacity
         self.brake_deadband = brake_deadband
         
-        
         self.last_time      = rospy.get_time()
 
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+        
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+        
+        current_vel     = self.vel_lpf.filt(current_vel)
+        steering        = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        vel_error       = linear_vel - current_vel
+        self.last_vel   = current_vel
+
+        current_time    = rospy.get_time()
+        sample_time     = current_time - self.last_time
+        self.last_time  = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+
+        if linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 700 # N*m to hold car in place if we are stopped at light. Acc - 1m/s^2
+
+        elif throttle < .1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel)*self.vehicle_mass*self.wheel_radius # Torque N*m
+
+        return throttle, brake, steering
