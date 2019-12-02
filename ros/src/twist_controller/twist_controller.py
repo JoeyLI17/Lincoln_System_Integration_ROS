690911154d7274d7c3c4a365eda58d8300d88d25
Joey Li <lijiayi17@gmail.com>
2019-12-01 23:32:55 -0500

    __init__ arguments

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -3,16 +3,15 @@ from lowpass import LowPassFilter
 from yaw_controller import YawController
 import rospy
 
-
-
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit,
+                wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
-        pass
+
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
